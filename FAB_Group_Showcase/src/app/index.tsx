cd2cf45a1b17a92f2d17614e690ee43edbaad2ce
EricPieTech <eric.pajtek@gmail.com>
2026-09-17 10:50:51 -0500

    FAB_Group from example

--- FAB_Group_Showcase/src/app/index.tsx
+++ FAB_Group_Showcase/src/app/index.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import FAB_Group from "./components/FAB_Group";
 //npm install react-native-paper
 //npm install react-native-safe-area-context
 //npm install @react-native-vector-icons/material-design-icons
@@ -6,6 +7,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>FAB.Group Example</Text>
+      <FAB_Group />
     </View>
   );
 }
